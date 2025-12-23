0b17e72daf1e115cbc5d27798b69e6a26b1a67a3
alishbaMalik395-adii <tumhara-github-email@gmail.com>
2025-12-23 23:13:11 +0500

    Update App.js and package files

--- App.js
+++ App.js
@@ -16,6 +16,21 @@ import UserDashboardScreen from './screens/UserDashboardScreen';
 import BusSelectionScreen from './screens/BusSelectionScreen';
 import DriverLiveMapScreen from './screens/DriverLiveMapScreen';
 
+// USER FLOW SCREENS
+import ChallanScreen from './screens/ChallanScreen';
+import ViewChallanScreen from './screens/ViewChallanScreen';
+import DownloadChallan from './screens/DownloadChallan';
+
+import PaymentScreen from './screens/PaymentScreen';
+import PaymentProcessingScreen from './screens/PaymentProcessingScreen';
+import PaymentSuccessScreen from './screens/PaymentSuccessScreen';
+
+import TrackingScreen from './screens/TrackingScreen';
+import MessagesScreen from './screens/MessagesScreen';
+import ComplaintScreen from './screens/ComplaintScreen';
+import ComplaintSuccessScreen from './screens/ComplaintSuccessScreen';
+import UserProfileScreen from './screens/UserProfileScreen';
+
 // ===== CONTEXT =====
 import { RideProvider } from './context/RideContext';
 
@@ -29,7 +44,8 @@ export default function App() {
           initialRouteName="Splash"
           screenOptions={{ headerShown: false }}
         >
-          {/* AUTH FLOW */}
+
+          {/* AUTH */}
           <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="AdminLogin" component={AdminLoginScreen} />
@@ -43,6 +59,21 @@ export default function App() {
           {/* DRIVER FLOW */}
           <Stack.Screen name="BusSelectionScreen" component={BusSelectionScreen} />
           <Stack.Screen name="DriverLiveMap" component={DriverLiveMapScreen} />
+
+          {/* USER FLOW */}
+          <Stack.Screen name="Challan" component={ChallanScreen} />
+          <Stack.Screen name="ViewChallan" component={ViewChallanScreen} />
+          <Stack.Screen name="DownloadChallan" component={DownloadChallan} />
+
+          <Stack.Screen name="Payment" component={PaymentScreen} />
+          <Stack.Screen name="PaymentProcessing" component={PaymentProcessingScreen} />
+          <Stack.Screen name="PaymentSuccess" component={PaymentSuccessScreen} />
+
+          <Stack.Screen name="TrackingScreen" component={TrackingScreen} />
+          <Stack.Screen name="MessagesScreen" component={MessagesScreen} />
+          <Stack.Screen name="ComplaintScreen" component={ComplaintScreen} />
+          <Stack.Screen name="ComplaintSuccess" component={ComplaintSuccessScreen} />
+          <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} />
 
         </Stack.Navigator>
       </NavigationContainer>
